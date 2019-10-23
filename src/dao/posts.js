f91a8f0df300392fc7433e4bc19280d8fbe75cae
achi <achievendar@gmail.com>
2019-10-23 11:32:47 +0300

    renamed variables

--- src/dao/posts.js
+++ src/dao/posts.js
@@ -2,9 +2,10 @@ const mysql = require('../database/mysql');
 
 const TOP_POSTS_NUMBER = 3;
 // TODO insert into errors db
-exports.create = async (uuid, text, createdAt, votes) => {
+// TODO add sanity tests
+exports.create = async (postId, text, createdAt, votes) => {
     try {
-        await mysql.query(`insert into posts (id, text, createdAt, votes) values ('${uuid}', '${text}', FROM_UNIXTIME(${createdAt}/1000), ${votes})`);//TODO use knex}
+        await mysql.query(`insert into posts (id, text, createdAt, votes) values ('${postId}', '${text}', FROM_UNIXTIME(${createdAt}/1000), ${votes})`);//TODO use knex}
         return true;
     } catch (err) {
         console.log(err.message);
@@ -12,9 +13,9 @@ exports.create = async (uuid, text, createdAt, votes) => {
     }
 };
 
-exports.update = async (uuid, text) => {
+exports.update = async (postId, text) => {
     try {
-        await mysql.query(`update posts set text='${text}' where id='${uuid}'`);
+        await mysql.query(`update posts set text='${text}' where id='${postId}'`);
         return true;
     } catch (err) {
         console.log(err.message);
@@ -22,15 +23,15 @@ exports.update = async (uuid, text) => {
     }
 };
 
-exports.vote = async (uuid, isUpvote) => {
+exports.vote = async (postId, isUpvote) => {
     try {
         const sign = isUpvote ? '+' : '-';
         await mysql.beginTransaction();
-        await mysql.query(`select @points := votes, @time := unix_timestamp(createdAt)/3600, @gravity := 1.8 from posts where id='${uuid}' for update`);
-        await mysql.query(`update posts set votes=(select @points${sign}1) where id='${uuid}'`);
+        await mysql.query(`select @points := votes, @time := unix_timestamp(createdAt)/3600, @gravity := 1.8 from posts where id='${postId}' for update`);
+        await mysql.query(`update posts set votes=(select @points${sign}1) where id='${postId}'`);
         await mysql.query('select * from top_posts for update');
         await mysql.query(`select @score := (select (@points))`);
-        await mysql.query(`insert into top_posts (id, score) values('${uuid}', @score) on duplicate key update score = @score`);
+        await mysql.query(`insert into top_posts (id, score) values('${postId}', @score) on duplicate key update score = @score`);
         await mysql.query(`select @lowest_id := id from top_posts order by score limit 1 for update`);
         await mysql.query(`select @count := count(*) from top_posts`);
         await mysql.query(`delete from top_posts where id = @lowest_id and @count = ${TOP_POSTS_NUMBER+2}`);
