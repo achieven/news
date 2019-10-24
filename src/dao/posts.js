8102096d8c0edf3a74e554e179b3ec2bf6a79851
achi <achievendar@gmail.com>
2019-10-24 04:24:35 +0300

    -return result to client -refactor -restired formula

--- src/dao/posts.js
+++ src/dao/posts.js
@@ -3,9 +3,11 @@ const mysql = require('../database/mysql');
 const TOP_POSTS_NUMBER = 3;
 // TODO insert into errors db
 // TODO add sanity tests
+// TODO use knex
+// TODO set transaction to read committed?
 exports.create = async (postId, text, createdAt, votes) => {
     try {
-        await mysql.query(`insert into posts (id, text, createdAt, votes) values ('${postId}', '${text}', FROM_UNIXTIME(${createdAt}/1000), ${votes})`);//TODO use knex}
+        await mysql.query(`insert into posts (id, text, createdAt, votes) values ('${postId}', '${text}', FROM_UNIXTIME(${createdAt}/1000), ${votes})`);
         return true;
     } catch (err) {
         console.log(err.message);
@@ -13,7 +15,7 @@ exports.create = async (postId, text, createdAt, votes) => {
     }
 };
 
-exports.update = async (postId, text) => {
+exports.edit = async (postId, text) => {
     try {
         await mysql.query(`update posts set text='${text}' where id='${postId}'`);
         return true;
@@ -30,7 +32,7 @@ exports.vote = async (postId, isUpvote) => {
         await mysql.query(`select @points := votes, @time := unix_timestamp(createdAt)/3600, @gravity := 1.8 from posts where id='${postId}' for update`);
         await mysql.query(`update posts set votes=(select @points${sign}1) where id='${postId}'`);
         await mysql.query('select * from top_posts for update');
-        await mysql.query(`select @score := (select (@points))`);
+        await mysql.query(`select @score := (select (@points-1)/(power(@time+2,@gravity)))`);
         await mysql.query(`insert into top_posts (id, score) values('${postId}', @score) on duplicate key update score = @score`);
         await mysql.query(`select @lowest_id := id from top_posts order by score limit 1 for update`);
         await mysql.query(`select @count := count(*) from top_posts`);
@@ -46,7 +48,8 @@ exports.vote = async (postId, isUpvote) => {
 
 exports.topPosts = async () => {
     try {
-        await mysql.query(`select post.text, post.votes from top_posts top_post join posts post on top_post.id = post.id order by top_post.score`)
+        const topPosts = await mysql.query(`select post.text, post.votes from top_posts top_post join posts post on top_post.id = post.id order by top_post.score desc limit ${TOP_POSTS_NUMBER}`);
+        return topPosts[0];
     } catch (err) {
         console.log(err.message);
         throw err;
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,14 +3,14 @@ const router  = express.Router();
 
 const postsController = require('../controllers/posts')
 //TODO route validation
+//TODO return to client only known errors
 router.post('/', async (req, res) => {
     const text = req.body.text;
     try {
-        await postsController.create(text);
-        return res.json({error: false, content: true});
+        const response = await postsController.create(text);
+        return res.json({error: false, content: response});
     } catch (err) {
-        console.log(err.message);
-        return res.status(500).json({error: true, content: "Internal Server Error"});//TODO put in const
+        return res.status(500).json({error: true, content: "Internal Server Error"});
     }
 });
 
@@ -18,10 +18,9 @@ router.put('/:postId', async (req, res) => {
     const postId = req.params.postId;
     const text = req.body.text;
     try {
-        await postsController.update(postId, text);
-        return res.json({error: false, content: true});
+        const response = await postsController.edit(postId, text);
+        return res.json({error: false, content: response});
     } catch (err) {
-        console.log(err.message);
         return res.status(500).json({error: true, content: "Internal Server Error"});
     }
 });
@@ -29,8 +28,8 @@ router.put('/:postId', async (req, res) => {
 router.put('/upvote/:postId', async (req, res) => {
     const postId = req.params.postId;
     try {
-        await postsController.upvote(postId);
-        return res.json({error: false, content: true});
+        const response = await postsController.upvote(postId);
+        return res.json({error: false, content: response});
     } catch (err) {
         return res.status(500).json({error: true, content: "Internal Server Error"});
     }
@@ -39,8 +38,8 @@ router.put('/upvote/:postId', async (req, res) => {
 router.put('/downvote/:postId', async (req, res) => {
     const postId = req.params.postId;
     try {
-        await postsController.downvote(postId);
-        return res.json({error: false, content: true});
+        const response = await postsController.downvote(postId);
+        return res.json({error: false, content: response});
     } catch (err) {
         console.log(err.message);
         return res.status(500).json({error: true, content: "Internal Server Error"});
@@ -49,8 +48,8 @@ router.put('/downvote/:postId', async (req, res) => {
 
 router.get('/top-posts', async (req, res) => {
   try {
-    await postsController.topPosts();
-    return res.json({error: false, content: true});
+      const response = await postsController.topPosts();
+    return res.json({error: false, content: response});
   } catch (err) {
       console.log(err.message);
       return res.status(500).json({error: true, content: "Internal Server Error"});
